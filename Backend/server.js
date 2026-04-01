cb782d86a4ee34802db9f3f64ea37d94d805cc90
madhavi <madhavitigireddi1@gmail.com>
2026-04-01 18:21:36 +0530

    token changes in signup

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,8 @@ dotenv.config();
 const connectDB = require("./src/config/db.js");
 connectDB();
 const authRoutes = require("./src/routes/auth.routes");
-const videoRoutes = require("./src/routes/video.routes.js")
+const videoRoutes = require("./src/routes/video.routes.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json())
@@ -13,6 +14,11 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('helo')
 })
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/videos', videoRoutes);
